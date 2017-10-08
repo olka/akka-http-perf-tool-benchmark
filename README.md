1d794d42644fce68c6e5ecc3bb92f1eef2ba5f88
alexander-kachur <alexander.kachur@gmail.com>
2017-10-08 18:36:14 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+Basic web app for load tools benchmarking
+Supports http and web sockets
---
+++ src/main/scala/org/example/CounterService.scala
@@ -0,0 +1,33 @@
+package org.example
+
+import java.util.concurrent.atomic.AtomicLong
+
+import akka.http.scaladsl.model._
+import akka.http.scaladsl.model.ws.{BinaryMessage, Message, TextMessage}
+import akka.http.scaladsl.server.Directives.{parameters, _}
+import akka.stream.scaladsl.{Flow, Source}
+
+object CounterService {
+
+  var counter = new AtomicLong(0);
+
+  def wsGreeter: Flow[Message, Message, Any] =
+    Flow[Message].mapConcat {
+      case tm: TextMessage =>
+        TextMessage(Source.single(getCounter) ++ tm.textStream) :: Nil
+      case bm: BinaryMessage => Nil
+    }
+
+  def getCounter:String = {counter.incrementAndGet().toString}
+
+  val route =
+    pathPrefix("counter") {
+      pathEnd {
+        get {
+          complete(HttpResponse(StatusCodes.OK, entity = HttpEntity(ContentType(MediaTypes.`application/json`), getCounter)))
+        }
+      }
+    } ~ path("wscounter") {
+      handleWebSocketMessages(wsGreeter)
+    }
+}
